d8368f24662805409c43978a4a886de7ed550fa7
Ashutosh Sharma <sharmaashutosh12355@gmail.com>
2026-04-11 13:27:35 +0530

    custom UserDetails

--- src/main/java/com/ashu/SpringSecurity/SpringSecurityApplication.java
+++ src/main/java/com/ashu/SpringSecurity/SpringSecurityApplication.java
@@ -1,7 +1,12 @@
 package com.ashu.SpringSecurity;
 
+import com.ashu.SpringSecurity.entity.Users;
+import com.ashu.SpringSecurity.repository.UserDetailsRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +15,15 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserDetailsRepository userDetailsRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			Users user = new Users();
+			user.setUsername("admin");
+			user.setPassword(passwordEncoder.encode("12345"));
+			user.setRole("ROLE_ADMIN");
+
+			userDetailsRepository.save(user);
+		};
+	}
 }
